e90ae9ec48f5a8052fdaf5b752223d58d2c9ff78
Andy Oakley <andy@andyoakley.com>
2018-09-20 06:14:09 +0000

    Update to mkdocs v1

--- mkdocs_nbconvert/nbconvert.py
+++ mkdocs_nbconvert/nbconvert.py
@@ -1,4 +1,5 @@
 from mkdocs.plugins import BasePlugin
+from mkdocs import utils
 import os.path
 from nbconvert import MarkdownExporter
 import nbformat
@@ -8,6 +9,8 @@ class NotebookConverter(BasePlugin):
 
     def __init__(self):
         self.exporter = MarkdownExporter()
+        if not '.ipynb' in utils.markdown_extensions:
+            utils.markdown_extensions.append('.ipynb')
 
     def can_load(self, path):
         return path.lower().endswith('.ipynb') and not 'ipynb_checkpoints' in path.lower()
@@ -18,31 +21,29 @@ class NotebookConverter(BasePlugin):
     def on_page_read_source(self, something, **kwargs):
         page = kwargs['page']
         config = kwargs['config']
+        input_path = page.file.src_path
 
-        if not self.can_load(page.input_path):
+        if not self.can_load(input_path):
             return
-
-        ipynb_path = os.path.join(config['docs_dir'], page.input_path)
+        
+        ipynb_path = os.path.join(config['docs_dir'], input_path)
         nb = nbformat.read(ipynb_path, as_version=4)
 
         # we'll place the supporting files alongside the final HTML
-        stem = os.path.splitext(os.path.basename(page.input_path))[0]
-        exporter_resources = {'output_files_dir': stem} 
+        stem = os.path.join(os.path.splitext(os.path.basename(input_path))[0], '..')
+        exporter_resources = {'output_files_dir': stem}
         
         (body, resources) = self.exporter.from_notebook_node(nb,
             resources=exporter_resources)
             
         # folder in site may not have been created yet, create it so that we
         # can drop the support files in there
-        target_in_site = os.path.join(
-                    config['site_dir'],
-                    page.abs_url[1:])
-        os.makedirs(target_in_site, exist_ok=True)
+        target_in_site = os.path.dirname(page.file.abs_dest_path)
+        os.makedirs(os.path.join(target_in_site, stem), exist_ok=True)
 
         for output in resources['outputs'].keys():
             path = os.path.join(
                     target_in_site,
-                    '..',
                     output
                     )
 
